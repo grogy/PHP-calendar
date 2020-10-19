4ccbbd4a679877933ca8ca56eaa48047a768752d
Václav Makeš <info@makes.cz>
2020-10-19 17:13:32 +0200

    update Nette Tester to v2

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -4,3 +4,5 @@ require __DIR__ . '/../vendor/autoload.php';
 require __DIR__ . '/../src/Calendar.php';
 require __DIR__ . '/../src/holiday/IHoliday.php';
 require __DIR__ . '/../src/holiday/CzechHoliday.php';
+
+\Tester\Environment::setup();
